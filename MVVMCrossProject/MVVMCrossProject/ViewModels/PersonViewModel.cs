071896243f80f38bffdfc0b3001ffd72b7cc2501
Ishrak El Andaloussi <ishrakland@outlook.com>
2018-07-11 14:30:51 +0100

    Add project Folders
    Signed-off-by: Ishrak El Andaloussi <ishrakland@outlook.com>

---
+++ MVVMCrossProject/MVVMCrossProject/ViewModels/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMCrossProject.ViewModels
+{
+    public class PersonViewModel
+    {
+
+    }
+}
---
+++ MVVMCrossProject/MVVMCrossProject/Views/Person.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMCrossProject.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Person : ContentPage
+	{
+		public Person ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
